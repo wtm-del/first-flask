98c96a3b3c30a7561a99123d057044374281636f
wtm-del <72029005+wtm-del@users.noreply.github.com>
2020-10-14 15:13:11 +0800

    showed name from url parameter in template

--- server.py
+++ server.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-   return render_template('index.html')
-
 @app.route("/another")
 def show():
     return '<h1>Yo</h1>'
@@ -12,6 +8,11 @@ def show():
 @app.route('/user/<username>')
 def insert(username):
     return f"Hi {username[2]}"
+
+@app.route("/<name>")
+def index(name):
+    name = name.upper()
+    return render_template('index.html', name=name)
 
 if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
    app.run(debug=True)
