d58cd6c94b54a1e8fb42bff1d4ee5e1dc1f8c8ad
wtm-del <72029005+wtm-del@users.noreply.github.com>
2020-10-14 15:26:27 +0800

    added sign in and sign out button

--- server.py
+++ server.py
@@ -9,10 +9,10 @@ def show():
 def insert(username):
     return f"Hi {username[2]}"
 
-@app.route("/<name>")
-def index(name):
-    name = name.upper()
-    return render_template('index.html', name=name)
+@app.route("/")
+def index():
+    signed_in = False
+    return render_template('index.html', signed_in=signed_in)
 
 if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
    app.run(debug=True)
