27bffabd25cc17e750be3baf09e0f09aab2472b8
wtm-del <72029005+wtm-del@users.noreply.github.com>
2020-10-14 14:59:45 +0800

    made show route show username

--- server.py
+++ server.py
@@ -9,5 +9,9 @@ def index():
 def show():
     return '<h1>Yo</h1>'
 
+@app.route('/user/<username>')
+def insert(username):
+    return f"Hi {username[0:3]}"
+
 if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
    app.run()
